ab1e9287d2c7ba4b07f183833c7ebc0086dabfdf
Modder <60491189+tap6386@users.noreply.github.com>
2020-12-27 12:29:16 -0800

    Custom Item Group (Creative Tab)

--- src/main/java/com/technovision/tutorial/Tutorial.java
+++ src/main/java/com/technovision/tutorial/Tutorial.java
@@ -3,10 +3,31 @@ package com.technovision.tutorial;
 import com.technovision.tutorial.registry.ModBlocks;
 import com.technovision.tutorial.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 
 public class Tutorial implements ModInitializer {
 
     public static final String MOD_ID = "tutorial";
+
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+            new Identifier(MOD_ID, "general"),
+            () -> new ItemStack(ModItems.RUBY));
+
+    public static final ItemGroup OTHER_GROUP = FabricItemGroupBuilder.create(
+            new Identifier(MOD_ID, "other"))
+            .icon(() -> new ItemStack(Blocks.ENCHANTING_TABLE))
+            .appendItems(stacks -> {
+                stacks.add(new ItemStack(ModBlocks.RUBY_BLOCK));
+                stacks.add(new ItemStack(Items.APPLE));
+                stacks.add(new ItemStack(ModItems.RUBY));
+                stacks.add(new ItemStack(Items.GLOWSTONE_DUST));
+            })
+            .build();
 
     @Override
     public void onInitialize() {
--- src/main/java/com/technovision/tutorial/registry/ModItems.java
+++ src/main/java/com/technovision/tutorial/registry/ModItems.java
@@ -10,10 +10,10 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
     //Items
-    public static final Item RUBY = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item RUBY = new Item(new Item.Settings().group(Tutorial.ITEM_GROUP));
 
     //Block Items
-    public static final BlockItem RUBY_BLOCK = new BlockItem(ModBlocks.RUBY_BLOCK, new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final BlockItem RUBY_BLOCK = new BlockItem(ModBlocks.RUBY_BLOCK, new Item.Settings().group(Tutorial.ITEM_GROUP));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(Tutorial.MOD_ID, "ruby"), RUBY);
