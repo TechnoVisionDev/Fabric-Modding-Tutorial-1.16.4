4123112da82a329bef197e9354510f6eb21e3ec5
Modder <60491189+tap6386@users.noreply.github.com>
2020-12-29 18:58:42 -0800

    Injecting loot pools into vanilla loot tables

--- src/main/java/com/technovision/tutorial/Tutorial.java
+++ src/main/java/com/technovision/tutorial/Tutorial.java
@@ -4,16 +4,29 @@ import com.technovision.tutorial.registry.ModBlocks;
 import com.technovision.tutorial.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.loot.ConstantLootTableRange;
+import net.minecraft.loot.LootTableRange;
+import net.minecraft.loot.UniformLootTableRange;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootTableEntry;
+import net.minecraft.loot.function.ApplyBonusLootFunction;
+import net.minecraft.loot.function.LootFunctionType;
+import net.minecraft.loot.function.LootFunctionTypes;
+import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.util.Identifier;
 
 public class Tutorial implements ModInitializer {
 
+    //Mod ID
     public static final String MOD_ID = "tutorial";
 
+    //Item Groups
     public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
             new Identifier(MOD_ID, "general"),
             () -> new ItemStack(ModItems.RUBY));
@@ -29,9 +42,33 @@ public class Tutorial implements ModInitializer {
             })
             .build();
 
+    //Loot Tables
+    private static final Identifier EMERALD_ORE_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/emerald_ore");
+    private static final Identifier MOD_EMERALD_ORE_LOOT_TABLE_ID = new Identifier(MOD_ID, "blocks/emerald_ore");
+
     @Override
     public void onInitialize() {
+        //Registration
         ModItems.registerItems();
         ModBlocks.registerItems();
+
+        //Loot Pools
+        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+            if (EMERALD_ORE_LOOT_TABLE_ID.equals(id)) {
+
+                // Add individual drop
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootTableRange.create(1))
+                        .with(ItemEntry.builder(Items.GOLD_NUGGET)) // Add individual drop
+                        .withFunction(SetCountLootFunction.builder(UniformLootTableRange.between(1.0f, 4.0f)).build());
+                supplier.withPool(poolBuilder.build());
+
+                // Inject custom loot table
+                FabricLootPoolBuilder poolBuilder2 = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootTableRange.create(1))
+                        .with(LootTableEntry.builder(MOD_EMERALD_ORE_LOOT_TABLE_ID));
+                supplier.withPool(poolBuilder2.build());
+            }
+        });
     }
 }
